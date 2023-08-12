86d67b83fd7e68399a2d726542a6ff12acf8f391
Carlos Azuaje <carlosjazzc1@gmail.com>
2023-08-12 02:32:07 -0300

    Finishing input file proccesing

--- src/lib/PrettyRestDoc.tsx
+++ src/lib/PrettyRestDoc.tsx
@@ -7,7 +7,10 @@ import React, {
   useState,
 } from "react";
 import { SECTION_ID } from "../constants";
-import AdaptarOA3 from "./adapter-oa3/adapter/Adapter";
+import {
+  AdaptarOA3,
+  AdaptarOA3FromFileInput,
+} from "./adapter-oa3/adapter/Adapter";
 import style from "./api.module.scss";
 import Header from "./components/Header";
 import { Navigation } from "./components/Navigation";
@@ -15,20 +18,29 @@ import { SearchModal } from "./components/SearchModal";
 import Section from "./components/Section";
 
 interface Props {
-  docSwagger: OpenAPIV3.Document;
-  docCustom: SectionItem[];
+  docSwagger?: OpenAPIV3.Document;
+  docCustom?: SectionItem[];
   roles?: Role[];
+  fileInput?: boolean;
 }
 
-const PrettyRestDoc: FC<Props> = ({ docCustom, docSwagger, roles }) => {
+const PrettyRestDoc: FC<Props> = ({
+  docCustom,
+  docSwagger,
+  fileInput,
+  roles,
+}) => {
   const [APIDoc, setAPIDOC] = useState<SectionItem[]>([]);
   const [section, setSection] = useState<string>("");
   const [searchModal, setSearchModal] = useState<boolean>(false);
 
-  const initDoc = async () => {
-    const adapter = new AdaptarOA3(docSwagger, docCustom);
-    const docMerged = await adapter.createDocumentation();
-    setAPIDOC(docMerged);
+  // Initialize the local doc that the repository contains (petstore.json)
+  const initLocalDoc = async () => {
+    if (docSwagger && docCustom) {
+      const adapter = new AdaptarOA3(docSwagger, docCustom);
+      const docMerged = await adapter.createDocumentation();
+      setAPIDOC(docMerged);
+    }
   };
 
   const isChromeExtensionMode =
@@ -52,7 +64,7 @@ const PrettyRestDoc: FC<Props> = ({ docCustom, docSwagger, roles }) => {
   }, []);
 
   useEffect(() => {
-    initDoc();
+    initLocalDoc();
     document.title = "Pretty Rest Doc";
 
     const eventsHandlers: any = { keydown: null, keyup: null };
@@ -131,9 +143,90 @@ const PrettyRestDoc: FC<Props> = ({ docCustom, docSwagger, roles }) => {
     index++;
   }
 
+  const getMissingAttributes = (jsonParsed: any): string[] => {
+    const requiredAttributes = [
+      "openapi",
+      "info",
+      "info.title",
+      "tags",
+      "paths",
+      "components",
+    ];
+
+    const missingAttributes: string[] = [];
+    for (const attr of requiredAttributes) {
+      const attrParts = attr.split(".");
+      let currentObj = jsonParsed;
+      let isValid = true;
+      for (const part of attrParts) {
+        if (!(part in currentObj)) {
+          isValid = false;
+          break;
+        }
+        currentObj = currentObj[part];
+      }
+      if (!isValid) {
+        missingAttributes.push(attr);
+      }
+    }
+    return missingAttributes;
+  };
+
+  const reason = (attr: string): string => {
+    const reasons: Record<string, string> = {
+      openapi: "It defines the OpenAPI version of the document.",
+      info: "It provides metadata about the API.",
+      "info.title": "It specifies the title of the API.",
+      tags: "It categorizes API operations into groups.",
+      paths: "It defines the available API endpoints.",
+      components: "It holds reusable components used in the specification.",
+    };
+    return reasons[attr] || "Unknown reason.";
+  };
+
+  const parseOpenAPIFile = async (file: File) => {
+    try {
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        try {
+          const fileContent = event.target?.result as string;
+          const jsonParsed = JSON.parse(fileContent);
+
+          const missingAttributes = getMissingAttributes(jsonParsed);
+
+          if (missingAttributes.length > 0) {
+            const missingAttributesMsg = missingAttributes
+              .map(
+                (attr) =>
+                  `Critical attribute: "${attr}" is not in the JSON File\nYou must add it in order to render it.\nThis value is important because: ${reason(
+                    attr
+                  )}.\n`
+              )
+              .join("\n");
+            alert(missingAttributesMsg);
+          } else {
+            // Call AdaptarOA3 with jsonParsed
+            const adapter = new AdaptarOA3FromFileInput(jsonParsed);
+            const docMerged = await adapter.createDocumentation();
+            setAPIDOC(docMerged);
+          }
+        } catch (error) {
+          alert(`Error parsing JSON file.\nError:\n${error}`);
+        }
+      };
+      reader.readAsText(file);
+    } catch (error) {
+      alert("Error reading file.");
+    }
+  };
+
   return (
     <>
-      <Header openSearchModal={openSearchModal} />
+      <Header
+        openSearchModal={openSearchModal}
+        fileInput={Boolean(fileInput)}
+        onFileUpload={(file) => parseOpenAPIFile(file)}
+      />
       <div className={style.Wrapper}>
         <SearchModal
           open={searchModal}
@@ -144,7 +237,7 @@ const PrettyRestDoc: FC<Props> = ({ docCustom, docSwagger, roles }) => {
           section={section}
           openSearchModal={openSearchModal}
           APIDoc={APIDoc}
-          docCustomOriginal={docCustom}
+          docCustomOriginal={docCustom || []}
         />
         <div className={style.Content}>{itemarray}</div>
       </div>
--- src/lib/adapter-oa3/adapter/Adapter.ts
+++ src/lib/adapter-oa3/adapter/Adapter.ts
@@ -7,18 +7,22 @@ import getArrayOfEndpoints from "./getArrayOfEndpoints";
 const resolver = new Resolver();
 
 class AdaptarOA3 {
-  private docSwagger: OpenAPIV3.Document;
-  private docCustom: SectionItem[];
+  protected docSwagger: OpenAPIV3.Document;
+  protected docCustom?: SectionItem[];
   constructor(
     // @ts-ignore
     docSwagger: OpenAPIV3.Document,
-    docCustom: SectionItem[]
+    docCustom?: SectionItem[]
   ) {
     this.docSwagger = docSwagger;
     this.docCustom = docCustom;
   }
 
   async createDocumentation(): Promise<SectionItem[]> {
+    if (!this.docCustom?.length) {
+      throw Error("You need send the docCustom");
+    }
+
     const swaggerDocResolved = await resolver.resolve(this.docSwagger);
     const docMerged: SectionItem[] = [];
     for (let i = 0; i < this.docCustom.length; i++) {
@@ -26,9 +30,8 @@ class AdaptarOA3 {
       const newSectionItemMerged: SectionItem = { ...sectionItem };
       if (sectionItem.is_core_resource) {
         if (sectionItem.schema) {
-          const schemaOfSection = this.docSwagger.components?.schemas?.[
-            sectionItem.schema
-          ];
+          const schemaOfSection =
+            this.docSwagger.components?.schemas?.[sectionItem.schema];
           if (schemaOfSection) {
             const objectSchema = getObjectSchema(
               swaggerDocResolved.result,
@@ -64,4 +67,55 @@ Please add the correct Schema called: ${sectionItem.schema}
   }
 }
 
-export default AdaptarOA3;
+class AdaptarOA3FromFileInput extends AdaptarOA3 {
+  constructor(docSwagger: OpenAPIV3.Document) {
+    super(docSwagger);
+  }
+
+  // Override the createDocumentation method to ignore docCustom
+  async createDocumentation(): Promise<SectionItem[]> {
+    const swaggerDocResolved = await resolver.resolve(this.docSwagger);
+    const docMerged: SectionItem[] = [];
+    // Iterate over the sections defined in the Swagger document
+    for (const sectionItem of swaggerDocResolved.result.paths) {
+      // Start here the fix
+      const newSectionItemMerged: SectionItem = { ...sectionItem };
+
+      if (sectionItem.schema) {
+        const schemaOfSection =
+          this.docSwagger.components?.schemas?.[sectionItem.schema];
+        if (schemaOfSection) {
+          const objectSchema = getObjectSchema(
+            swaggerDocResolved.result,
+            sectionItem.schema
+          );
+          newSectionItemMerged.object_schema = objectSchema;
+          const objectExample = getObjectExample(
+            swaggerDocResolved.result,
+            sectionItem.schema
+          );
+          newSectionItemMerged.object_example = objectExample;
+        } else {
+          console.warn(`
+  The documentation section
+  ${sectionItem.title}
+  Don't have the correct OA3 Schema in components attribute.
+  Please add the correct Schema called: ${sectionItem.schema}
+          `);
+        }
+      }
+      if (sectionItem.tag) {
+        newSectionItemMerged.endpoints = getArrayOfEndpoints(
+          swaggerDocResolved.result.paths,
+          sectionItem.tag,
+          swaggerDocResolved.result
+        );
+      }
+
+      docMerged.push(newSectionItemMerged);
+    }
+    return docMerged;
+  }
+}
+
+export { AdaptarOA3, AdaptarOA3FromFileInput };
